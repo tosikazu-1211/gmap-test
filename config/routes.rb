d9eb6a99807a2a6feda28295292d2bab5fb6e1a6
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-11-03 02:55:36 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'maps#index'
+  resources :maps, only: [:index]
+  get '/map_request', to: 'maps#map', as: 'map_request'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
